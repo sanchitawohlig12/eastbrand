54a4c8b62e1ac1d8a5b3549de7841d4c29ec6420
sanchitagithub12 <sanchitagithub@gmail.com>
2021-10-18 10:59:39 +0530

    done

--- components/Home3.vue
+++ components/Home3.vue
@@ -60,7 +60,7 @@
             <div class="office">
               <v-icon style="color: orange">mdi-map-marker</v-icon>
 
-              Ace Worldwide Limited,6 Roedean Avenue,Enfield, ,EN3 5QJ
+              Ace Worldwide Limited,6 Roedean Avenue,Enfield, EN3 5QJ
             </div>
             <div class="office">
               <v-icon style="color: orange">mdi-phone</v-icon>
@@ -139,7 +139,7 @@
               <div class="office">
                 <v-icon style="color: orange">mdi-map-marker</v-icon>
 
-                Ace Worldwide Limited,6 Roedean Avenue,Enfield,EN3 5QJ
+                Ace Worldwide Limited,6 Roedean Avenue, Enfield, EN3 5QJ
               </div>
               <div class="office">
                 <v-icon style="color: orange">mdi-phone</v-icon>
